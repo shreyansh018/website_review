4a98d952e4e7fbead3df2f54230844f7efe8a60d
shreyansh018 <shreyansh.singhrajput2019@gmail.com>
2025-03-05 22:17:18 +0530

    finalize code

--- review/urls.py
+++ review/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('submit-url', views.submit_url, name='submit-url'),
+    path('feedback', views.feedback, name='feedback')
 ]
